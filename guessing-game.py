aa3bbf3c40aebfdcd6c51e293c1cc8af1d07d9bc
Olga <ko3.olga@gmail.com>
2022-10-15 23:22:01 +0300

    update guessing-game.py PySimpleGUI

--- guessing-game.py
+++ guessing-game.py
@@ -1,11 +1,7 @@
 import random
+import PySimpleGUI as sg
 
-print('Добро пожаловать в числовую угадайку!')
-
-limit_min = input('Укажите, в каком диапазоне Вы готовы угадывать числа.\nВведите минимальное в пределах от 1 до 1000: ')
-limit_max = input('Введите максимальное число в пределах от 1 до 1000: ')
-
-random_num = random.randint(int(limit_min), int(limit_max) + 1)
+sg.theme('DarkAmber')
 
 def is_valid(s):
     if s.isdigit() and 1 <= int(s) <= 1000:
@@ -13,27 +9,34 @@ def is_valid(s):
     else:
         return False
 
-count_of_attempt = 0
+sg.popup('Добро пожаловать в числовую угадайку!')
+
+limit_min = int(sg.popup_get_text('Укажите, в каком диапазоне Вы готовы угадывать числа.\nВведите минимальное в пределах от 1 до 1000: '))
+limit_max = int(sg.popup_get_text('Введите максимальное число в пределах от 1 до 1000: '))
+random_num = random.randint(int(limit_min), int(limit_max) + 1)
+
+count_of_attempt = 1
 
 while True:
-    number = input(f'Введите целое число от {limit_min} до {limit_max}:   ')
+
+    number = sg.popup_get_text(f'Введите целое число от {limit_min} до {limit_max}:   ')
     if not is_valid(number):
-        print(f'А может быть все-таки введем целое число от {limit_min} до {limit_max}?')
+        sg.popup(f'А может быть все-таки введем целое число от {limit_min} до {limit_max}?')
         continue
     number = int(number)
     if number < random_num:
-        print('Ваше число меньше загаданного, попробуйте еще разок')
+        sg.popup('Ваше число меньше загаданного, попробуйте еще разок')
         count_of_attempt += 1
     elif number > random_num:
-        print('Ваше число больше загаданного, попробуйте еще разок')
+        sg.popup('Ваше число больше загаданного, попробуйте еще разок')
         count_of_attempt += 1
     elif number == random_num:
-        print(f'''Вы угадали, поздравляем!
+        sg.popup(f'''Вы угадали, поздравляем!
 Количество совершённых попыток: {count_of_attempt}''')
-        again = input(f'Хотите повторить игру? Да/Нет   ')
+        again = sg.popup_get_text(f'Хотите повторить игру? Да/Нет   ')
         if again == 'Нет':
             break
         else:
             continue
 
-print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
+sg.popup('Спасибо, что играли в числовую угадайку. Еще увидимся...')
